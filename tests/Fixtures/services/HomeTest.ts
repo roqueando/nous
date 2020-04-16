4510f3fca4b9a26db0218b9c6e0404b0698be59b
Vitor Roque <vitor.roque@netshow.me>
2020-04-15 21:52:36 -0300

    fix: JSON Error #1mxykj

--- tests/Fixtures/services/HomeTest.ts
+++ tests/Fixtures/services/HomeTest.ts
@@ -1,10 +1,18 @@
-
 import Service from '../../../src/core/Service'
 import Serviceable from '../../../src/contracts/Serviceable'
+import * as json from './db/db.json';
 
 export default class HomeTest extends Service implements Serviceable {
 
     public hello(name: string) {
         return ("Hello " + name);
+    }
+
+    public getJson() {
+        return json;
+    }
+
+    public getBool(): boolean{
+        return true;
     }
 }
